c127eb638dcf420b17bc5ce5a9130e234937add2
Notsponge <arevadzezu2009@gmail.com>
2024-12-14 18:30:09 +0200

    Add files via upload

---
+++ Sashinao JS-4/index.js
@@ -0,0 +1,70 @@
+//Task 1
+
+// let fullName = "John Doe"
+
+// let namePart = fullName.split(' ');
+
+// let initials = namePart.map(part => part.substring(0,1).toUpperCase()).join('');
+
+// console.log(initials)
+
+
+
+
+
+//Task 2
+
+// let Mychar = "D"
+// let char = Mychar.charCodeAt(0)
+
+
+// if (char >= 65 && char < 90){
+//   console.log("Uppercase")
+// }
+
+// else if (char >= 97 && char < 122){
+//   console.log("Lowercase")
+// }
+
+// else if (char >= 48 && char < 57){
+//   console.log("Digit")
+// }
+
+// else {
+//   console.log("unknown")
+// }
+
+
+
+
+//Task 4
+
+
+// let str = "hello";
+// let reversed = "";
+
+// for (let i = str.length-1 ; i >=0 ; i--){
+//   reversed+=str[i]
+// }
+
+
+// let string = "Zuka Arexa"
+
+// switch (true) {
+
+//   case(string.length <= 5):
+//   console.log("Short String")
+//   break;
+
+//   case(string.length >= 6 && string.length < 10):
+//   console.log("Medium String")
+//   break;
+
+//   case(string.length >= 10):
+//   console.log("Long String")
+//   break;
+
+//   default:
+//   console.log("Unknown String Length");
+
+// }
